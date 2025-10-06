1f52440d5519f070aeee055be3213cf359b9e7b1
Justin van der Mey <justinvdmey@hotmail.com>
2025-10-06 23:01:36 +0200

    feat: Implementation of temp configuration flow

---
+++ custom_components/racepulse/config_flow.py
@@ -0,0 +1,30 @@
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+from homeassistant import config_entries
+
+from .const import DOMAIN
+
+
+class F1FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title=user_input["sensor_name"], data=user_input
+            )
+
+        data_schema = vol.Schema(
+            {
+                vol.Required("sensor_name", default="F1"): cv.string,
+                vol.Optional(
+                    "live_delay_seconds", default=0
+                ): vol.All(vol.Coerce(int), vol.Range(min=0, max=300)),
+            }
+        )
+
+        return self.async_show_form(
+            step_id="user", data_schema=data_schema, errors=errors
+        )
